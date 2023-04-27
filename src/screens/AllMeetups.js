cf39a5f9c801478f6efc56ac10054325ad69f30a
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-04-27 20:58:41 +0200

    Icons events

--- src/screens/AllMeetups.js
+++ src/screens/AllMeetups.js
@@ -11,7 +11,7 @@ import Card from "../shared/Card";
 import uuid from "react-native-uuid";
 import { globalStyles } from "../styles/global";
 
-const AllMeetups = () => {
+const AllMeetups = ({ navigation }) => {
   const [locations, setLocations] = useState([
     {
       id: uuid.v4(),
@@ -35,6 +35,21 @@ const AllMeetups = () => {
       favorite: false,
     },
   ]);
+
+  const goToDetails = (item) => {
+    navigation.navigate("MeetupDetails", item);
+  };
+
+  const toggleFavorite = (id) => {
+    const updatedLocations = locations.map((location) => {
+      if (location.id === id) {
+        return { ...location, favorite: !location.favorite };
+      }
+      return location;
+    });
+    setLocations(updatedLocations);
+  };
+
   return (
     <View style={globalStyles.container}>
       <FlatList
@@ -46,12 +61,15 @@ const AllMeetups = () => {
               name="information-circle"
               size={24}
               color="cornflowerblue"
+              onPress={() => goToDetails(item)}
             />
             {/* <Ionicons name="heart-outline" size={24} color="indianred" /> */}
             <Ionicons
-              name="heart-dislike-outline"
+              // name="heart-dislike-outline"
+              name={item.favorite ? "heart-outline" : "heart-dislike-outline"}
               size={24}
               color="indianred"
+              onPress={() => toggleFavorite(item.id)}
             />
           </Card>
         )}
