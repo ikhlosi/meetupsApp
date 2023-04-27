e2f6ed654baefde1ba9d2febd6f4aefc0ae4e89c
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-04-27 20:57:21 +0200

    Loading Fonts

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 import AllMeetups from "./src/screens/AllMeetups";
+import TabNav from "./src/routes/TabNav";
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    "cute-easter": require("./assets/fonts/CuteEasterPersonalUse-Wy8nV.ttf"),
+    "pacifico-regular": require("./assets/fonts/PacificoRegular-BXvV.ttf"),
+  });
+
+  if (!fontLoaded) {
+    return <Text>Loading fonts...</Text>;
+  }
+
   return (
-    <AllMeetups />
+    <TabNav />
     // <View style={styles.container}>
     //   <Text>Open up App.js to start working on your app!</Text>
     //   <StatusBar style="auto" />
