569cc3fcc7e64d71d0605e3ef9d1ab39beb59fa9
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-04-27 21:00:38 +0200

    Custom Header and screenOptions

--- src/routes/AboutStack.js
+++ src/routes/AboutStack.js
@@ -1,11 +1,22 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import About from "../screens/About";
+import Header from "../shared/Header";
+
+const Stack = createStackNavigator();
 
 const AboutStack = () => {
   return (
-    <View>
-      <Text>AboutStack</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="About"
+        component={About}
+        options={() => {
+          return { header: () => <Header title={"About"} /> };
+        }}
+      />
+    </Stack.Navigator>
   );
 };
 
--- src/routes/MeetupStack.js
+++ src/routes/MeetupStack.js
@@ -3,14 +3,38 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import AllMeetups from "../screens/AllMeetups";
 import MeetupDetails from "../screens/MeetupDetails";
+import Header from "../shared/Header";
 
 const Stack = createStackNavigator();
 
 const MeetupStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="AllMeetups" component={AllMeetups} />
-      <Stack.Screen name="MeetupDetails" component={MeetupDetails} />
+    <Stack.Navigator
+      screenOptions={{
+        headerTitleStyle: {
+          fontFamily: "pacifico-regular",
+          fontSize: 20,
+          color: "#333",
+          letterSpacing: 1,
+        },
+      }}
+    >
+      <Stack.Screen
+        name="AllMeetups"
+        component={AllMeetups}
+        options={() => {
+          return {
+            header: () => <Header title={"All Meetups"} />,
+          };
+        }}
+      />
+      <Stack.Screen
+        name="MeetupDetails"
+        component={MeetupDetails}
+        options={{
+          title: "Meetup Details",
+        }}
+      />
     </Stack.Navigator>
   );
 };
